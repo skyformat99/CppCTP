0015942ed144d3f671e1bf6af00d2dcd7e7aec7d
345161974 <ypf1990@foxmail.com>
2017-01-06 22:40:20 +0800

    2017/01/06 22:39:50 backup from windows, comments: 1:update the push.bat 2:solve some issues.

--- push.bat
+++ push.bat
@@ -1,10 +1,13 @@
+@echo off
 set d=%date:~0,10%
 set t=%time:~0,8%
 set timestamp=%d% %t%
 
+set /p comments=please input commit comments:
+
 git status
 git add .
-git commit -m "%timestamp% backup from windows"
+git commit -m "%timestamp% backup from windows, comments: %comments%"
 git push origin CppCTP_API_ypf
 echo "Finished Push!"
 pause
