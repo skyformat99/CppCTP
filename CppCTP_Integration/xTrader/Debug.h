6e11ea65ba4d36c2141d3e2a29f80a699dbf2138
345161974 <ypf1990@foxmail.com>
2017-01-18 22:19:47 +0800

    2017/01/18 22:19:40 backup from windows, comments: 更新了debug输出格式

--- CppCTP_Integration/xTrader/Debug.h
+++ CppCTP_Integration/xTrader/Debug.h
@@ -6,6 +6,7 @@
 #include <ctime>   // localtime
 #include <sstream> // stringstream
 #include <string>  // string
+#include <cstring>
 
 using namespace std;
 using namespace std::chrono;
@@ -13,7 +14,7 @@ using std::string;
 
 //#define DEBUG
 #ifdef DEBUG
-#define USER_PRINT(x) {time_t tt = system_clock::to_time_t(system_clock::now()); std::cerr << "[DEBUG] - " << std::ctime(&tt) << "" << __FILE__ << ", Line - " << __LINE__ << " - " << " = " << x << std::endl;}
+#define USER_PRINT(x) {time_t tt = system_clock::to_time_t(system_clock::now()); std::string nowt(std::ctime(&tt)); std::cerr << "[DEBUG] - " << nowt.substr(0, nowt.length() - 1) << ", " << __FILE__ << ", Line - " << __LINE__ << "_" << " = " << x << std::endl;}
 #else
 #define USER_PRINT(x)
 #endif
