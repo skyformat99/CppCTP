efc9e50ce5e9b4da76d710f07504bbca39fdcbe1
345161974 <ypf1990@foxmail.com>
2017-01-18 22:00:42 +0800

    2017/01/18 22:00:28 backup from windows, comments: 更新了debug版本时间戳,支持实时输出时间戳

--- CppCTP_Integration/xTrader/Debug.h
+++ CppCTP_Integration/xTrader/Debug.h
@@ -1,14 +1,21 @@
 #ifndef QUANT_CTP_DEBUG_H
 #define QUANT_CTP_DEBUG_H
+#include <iostream>
+#include <ratio>
+#include <chrono>  // chrono::system_clock
+#include <ctime>   // localtime
+#include <sstream> // stringstream
+#include <string>  // string
+
+using namespace std;
+using namespace std::chrono;
+using std::string;
 
 //#define DEBUG
 #ifdef DEBUG
-#define USER_PRINT(x) std::cerr << "[DEBUG] - " << __DATE__ << ", " << __TIME__<< ", " << __FILE__ << ", Line - " << __LINE__ << "-" << #x << " = " << x << std::endl;
+#define USER_PRINT(x) {time_t tt = system_clock::to_time_t(system_clock::now()); std::cerr << "[DEBUG] - " << std::ctime(&tt) << "" << __FILE__ << ", Line - " << __LINE__ << " - " << " = " << x << std::endl;}
 #else
 #define USER_PRINT(x)
 #endif
-
-using namespace std;
-using std::string;
 
 #endif
