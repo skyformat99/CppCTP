5a38aa910d1bec0085ee1f40935c70abc8bafcc8
345161974 <ypf1990@foxmail.com>
2017-02-13 11:08:56 +0800

    2017/02/13 11:08:45 backup from windows, comments: 更新输出update_status

--- CppCTP_Integration/xTrader/Strategy.cpp
+++ CppCTP_Integration/xTrader/Strategy.cpp
@@ -1213,6 +1213,7 @@ void Strategy::OnRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarket
 	USER_PRINT(this->stg_trade_tasking);
 	/// 如果有交易任务,进入交易任务执行
 	if (this->stg_trade_tasking) {
+		this->printStrategyInfo("有交易任务,进入交易任务执行");
 		this->Exec_OnTickComing(pDepthMarketData);
 	}
 	else { /// 如果没有交易任务，那么选择开始新的交易任务
